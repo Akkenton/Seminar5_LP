2987edb6b6b27d4c35acba86a45bcec88d8f4dde
Akkenton <akkenton.msk@gmail.com>
2022-12-05 21:11:27 +0300

    Первая сессионка

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,26 +1,57 @@
-﻿int[] array = new int[5];
-Random rand = new Random();
+﻿const int SIZE = 12;
+const int LEFTRANGE = -9;
+const int RIGHTRANGE = 9;
+int[] array = new int[SIZE];
 int sumPositive = 0;
 int sumNegative = 0;
 
-for (int i = 0; i < array.Length; i++)
+
+int[] FillArrayWithRandomNumbers(int size, int leftrange, int rightrange)
 {
-    array[i] = rand.Next(-9, 10);
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftrange, rightrange + 1);
+    }
+
+    return arr;
 }
 
+void SumPosAndNegNum(int[] arr, out int sumP, out int sumN)
+{
+    sumP = 0;
+    sumN = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < 0)
+        {
+            sumP += arr[i];
+        }
+        else
+        {
+            sumN += arr[i];
+        }
+    }
+}
+array = FillArrayWithRandomNumbers(SIZE, LEFTRANGE, RIGHTRANGE);
 Console.WriteLine("[" + string.Join(",", array) + "]");
 
-for (int i = 0; i < array.Length; i++)
-{
-    if (array[i] < 0)
-    {
-        sumPositive += array[i];
-    }
-    else
-    {
-        sumNegative += array[i];
-    }
-}
 
-Console.WriteLine($"Сумма положительных элементов = {sumPositive}");
-Console.WriteLine($"Сумма отрицательны элементов = {sumNegative}");
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] < 0)
+//     {
+//         sumPositive += array[i];
+//     }
+//     else
+//     {
+//         sumNegative += array[i];
+//     }
+// }
+
+// Console.WriteLine($"Сумма положительных элементов = {sumPositive}");
+// Console.WriteLine($"Сумма отрицательны элементов = {sumNegative}");
+
+SumPosAndNegNum(array, out int sumP, out int sumN);
+System.Console.WriteLine($"Сумма положительных элементов = {sumP}, отрицательных элементов = {sumN}");
--- 02/Program.cs
+++ 02/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+Console.Clear();
+
+
+// -------------- TASK_32
+// int size = 5;
+// int lRange = -9;
+// int rRange = 9;
+// int[] array = new int[size];
+
+
+// int[] FillArrayWithRandomNumbers(int size, int leftrange, int rightrange)
+// {
+//     Random rand = new Random();
+//     int[] arr = new int[size];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = rand.Next(leftrange, rightrange + 1);
+//     }
+
+//     return arr;
+// }
+
+// array = FillArrayWithRandomNumbers(size, lRange, rRange);
+// Console.WriteLine("[" + string.Join(",", array) + "]");
+
+// int[] ChangePosNeg(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] *= -1;
+//     }
+//     return arr;
+// }
+
+// array = ChangePosNeg(array);
+// Console.WriteLine("[" + string.Join(",", array) + "]");
+
+
+
+
+// -------------- TASK_33
+
+int[] FillArrayWithRandomNumbers(int size, int leftrange, int rightrange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftrange, rightrange + 1);
+    }
+
+    return arr;
+}
+
+void FindNum(int[] arr, int value)
+{
+    int flag = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == value)
+        {
+            flag = 1;
+            System.Console.WriteLine("Да!");
+            break;
+        }
+    }
+    if (flag == 0) System.Console.WriteLine("НЕТ!");
+}
+
+int size = 5;
+int lRange = -9;
+int rRange = 9;
+int[] array = new int[size];
+
+array = FillArrayWithRandomNumbers(size, lRange, rRange);
+Console.WriteLine("[" + string.Join(",", array) + "]");
+System.Console.WriteLine("Введите искомое в массиве число: ");
+int num = int.Parse(Console.ReadLine());
+FindNum(array, num);
